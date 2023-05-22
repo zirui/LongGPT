254d021e45b4ca071d488d2243d5ff2306406812
zirui <zirui.dream@gmail.com>
2023-05-22 15:49:17 +0800

    add scripts for download & preprocess data

---
+++ data/download_pile.sh
@@ -0,0 +1,13 @@
+
+#!/bin/bash
+start=0
+end=29
+mkdir data/the_pile
+for (( i=$start; i<=$end; i++ ))
+do
+    url="https://the-eye.eu/public/AI/pile/train/$(printf "%02d" $i).jsonl.zst"
+    echo "Downloading file: $url"
+    curl -C - $url -o data/the_pile/"$(printf "%02d" $i).jsonl.zst"
+done
+
+wait
